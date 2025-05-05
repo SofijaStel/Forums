7317fa0ddf552137583d6d35f4dfd9b171c19998
SofijaStel <144788471+SofijaStel@users.noreply.github.com>
2025-05-05 10:53:26 +0300

    Commit message
    Commit message

---
+++ script.js
@@ -0,0 +1,20 @@
+function loadComments() {
+    const container = document.getElementById('commentsContainer');
+    const comments = JSON.parse(localStorage.getItem('atsauksmes')) || [];
+  
+    if (comments.length === 0) {
+      container.innerHTML = "<p>Vēl nav atsauksmju.</p>";
+      return;
+    }
+  
+    comments.forEach(c => {
+      const div = document.createElement('div');
+      div.className = 'comment';
+      div.innerHTML = `<p>${c.comment}</p>
+                       <div class="meta">${c.name} – ${c.time}</div>`;
+      container.appendChild(div);
+    });
+  }
+  
+  window.addEventListener('load', loadComments);
+  
