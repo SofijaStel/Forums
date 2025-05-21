b540a76cbcb79d8b6fc5db370c99c0617138983f
SofijaStel <144788471+SofijaStel@users.noreply.github.com>
2025-05-22 00:56:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
+//Palīgmateriāls MDN Web Docs
 function loadComments() {
-    const container = document.getElementById('commentsContainer');
-    const comments = JSON.parse(localStorage.getItem('atsauksmes')) || [];
-  
-    if (comments.length === 0) {
-      container.innerHTML = "<p>Vēl nav atsauksmes.</p>";
-      return;
-    }
-  
-    comments.forEach(c => {
-      const div = document.createElement('div');
-      div.className = 'comment';
-      div.innerHTML = `<p>${c.comment}</p>
-                       <div class="meta">${c.name} – ${c.time}</div>`;
-      container.appendChild(div);
-    });
+  const container = document.getElementById('commentsContainer'); // Atrod <div> kur būs komentāri
+  const comments = JSON.parse(localStorage.getItem('atsauksmes')) || []; // Pārnes komentārus no localStorage
+
+  if (comments.length === 0) {
+    container.innerHTML = "<p>Vēl nav atsauksmes.</p>"; // Ja nav neviena, parāda paziņojumu
+    return;
   }
-  
-  window.addEventListener('load', loadComments);
-  
+
+  comments.forEach(c => {
+    const div = document.createElement('div'); // Izveido jaunu <div>
+    div.className = 'comment'; // Pievieno CSS klasi
+    div.innerHTML = `
+      <p>${c.comment}</p>
+      <div class="meta">${c.name} – ${c.time}</div>`; // Ievieto tekstu
+    container.appendChild(div); // Pievieno HTML dokumentā
+  });
+}
+
+window.addEventListener('load', loadComments); // Kad lapa ielādējas, izsauc funkciju
